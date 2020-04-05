d428c99fa276cdf34023fb3d14b0f8f0ad8a4868
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-04-05 20:14:46 +0900

    Fix all action in articles controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,27 @@
 class ApplicationController < ActionController::Base
 
   def index
+    @articles = Article.order("create_at DESC")
   end
 
   def new
+    @article = Article.new
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
-  
+
 end

7420909133b477d7e5f1e71dde8130e45addb0e8
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-04-05 20:17:57 +0900

    Fix article controller because edited wrong controller before

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,30 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.order("create_at DESC")
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:username, :description, :country)
+  end
+
 end
